3b85f2094aaeec786012d7e1bd19d6750edf90f0
Ivan <ivanvgav@gmail.com>
2025-04-23 21:11:26 -0300

    Add retos

---
+++ retos.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+*/
+
+// int main(){
+//     int i;
+//     char fizz[] = "fizz";
+//     char buzz[] = "buzz";
+//     char fizzbuzz[] = "fizzbuzz";
+    
+//     for(i = 1; i < 101; i++){
+//         if (i % 3 == 0 && i % 5 == 0){
+//             printf("%s\n", fizzbuzz);
+//         } else if (i % 5 == 0){
+//             printf("%s\n", buzz);
+//         } else if (i % 3 == 0 ){
+//             printf("%s\n", fizz);
+//         } else {
+//             printf("%d\n", i);
+//         }
+//     }
+//    return 0;
+// }
+
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+*/
